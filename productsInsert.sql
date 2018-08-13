c2991893ee2538e16c8abbe5461af5a819571bcf
iMAC 2015 <38024526+NitramDivad@users.noreply.github.com>
2018-08-13 01:20:43 -0600

    SQL inserts for initial Data

---
+++ productsInsert.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM bamazon.products
+LIMIT 0, 1000
+
+-- Date: 2018-08-13 01:18
+*/
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (1,'Echo Plus','Electronics',99.99,385,99.99);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (2,'Echo Show','Electronics',249.99,621,749.97);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (3,'Apple iPhone X','Cellular',999.99,415,0.00);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (4,'FRAM CF10134 Air Filter','Automotive',14.99,6,29.98);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (5,'Emergenc-C Immune+ (30 Count Super Orange)','Health',8.99,89,0.00);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (6,'Gold Toe Men\'s 6-Pack Cotton Crew Athletic Socks','Clothing',14.99,4,0.00);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (7,'Tide Mountain Spring HE Turbo Powder Laundry Detergent','Household',15.99,29,0.00);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (8,'Oakley Flight Deck XML Snow Goggles','Sports & Fitness',159.99,3,0.00);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (9,'Apple MMEF2AM/A Airpods','Electronics',144.98,9,0.00);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (10,'Holmes HAPF600 HEPA Filter','Home & Kitchen',22.49,88,0.00);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (11,'Staples','Office Supplies',3.99,999,0.00);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (12,'Patio Lounge Chair','Outdoor & Patio',247.99,42,0.00);
