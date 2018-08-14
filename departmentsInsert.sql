41e43289d5f870bd5750bbcbec919895eeabe564
iMAC 2015 <38024526+NitramDivad@users.noreply.github.com>
2018-08-13 22:58:10 -0600

    SQL insert for Departments

---
+++ departmentsInsert.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM departments
+LIMIT 0, 1000
+
+-- Date: 2018-08-13 01:19
+*/
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (1,'Electronics',2499.82);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (2,'Cellular',1500.00);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (3,'Automotive',975.00);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (4,'Health',475.50);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (5,'Clothing',778.85);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (6,'Household',1115.25);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (7,'Sports & Fitness',888.88);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (8,'Home & Kitchen',551.10);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (9,'Office Supplies',933.66);
+INSERT INTO `departments` (`department_id`,`department_name`,`over_head_costs`) VALUES (10,'Outdoor & Patio',1459.99);
